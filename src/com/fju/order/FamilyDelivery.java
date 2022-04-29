34d4efbfb567a77b9c873b3bc7e318e465fe2881
User <liguan181@gmail>
2022-04-29 16:04:23 +0800

    my answer

---
+++ src/com/fju/order/FamilyDelivery.java
@@ -0,0 +1,8 @@
+package com.fju.order;
+
+public class FamilyDelivery extends Delivery{
+    public FamilyDelivery(){
+        name = "family";
+        price=60;
+    }
+}
--- src/com/fju/order/Order.java
+++ src/com/fju/order/Order.java
@@ -3,9 +3,22 @@ package com.fju.order;
 public class Order {
     int amount;
     Delivery delivery;
+    int shipFeed;
 
     public Order(int amount, Delivery delivery) {
         this.amount = amount;
         this.delivery = delivery;
+        shipFeed=shipfeed();
     }
+    public int total(){
+        return (amount+ delivery.price);
+    }
+    public int shipfeed() {
+        if (delivery.name == "shopee" && delivery.price > 199){
+            return (0);
+        }else{
+           return (shipFeed=delivery.price);
+        }
+    }
+
 }
